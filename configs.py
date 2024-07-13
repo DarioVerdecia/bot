6a9c2afd8f7ed945b4623e47dfc5d996722456fc
DarioVerdecia <139101683+DarioVerdecia@users.noreply.github.com>
2024-07-13 18:54:02 -0400

    configs.py

--- configs.py
+++ configs.py
@@ -1,3 +1,3 @@
 api_id = 27919656
 api_hash = "e9509af119ab1ee5fb0df18ab04ee19c" #Esto se obtenien en mytelegramorg
-token = "7434538472:AAHMN1lk2jVVAXUeFti7ocR5r4A8-kWCfas" #Crea token del bot en botfhater
+token = "7106397793:AAEYK-UErnLNw0DxvMWNK0mvt1CTiYvaWoY" #Crea token del bot en botfhater
