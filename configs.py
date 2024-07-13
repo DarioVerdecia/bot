02bd69ef8f7daf0078367f40d2ab1dc7a6c8cec7
DarioVerdecia <139101683+DarioVerdecia@users.noreply.github.com>
2024-07-13 18:49:04 -0400

    Add files via upload

---
+++ configs.py
@@ -0,0 +1,3 @@
+api_id = 27919656
+api_hash = "e9509af119ab1ee5fb0df18ab04ee19c" #Esto se obtenien en mytelegramorg
+token = "7434538472:AAHMN1lk2jVVAXUeFti7ocR5r4A8-kWCfas" #Crea token del bot en botfhater
